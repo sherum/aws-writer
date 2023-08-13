cb66da59270c8afebc3482c37ab4b1169b7ecdb2
davidwoolery <sheurm@gmail.com>
2023-08-13 09:19:50 -0500

    update polyfills

--- plotter/src/app/app.module.ts
+++ plotter/src/app/app.module.ts
@@ -63,7 +63,7 @@ import {AuthModule} from "./auth/auth.module";
  ],
   imports: [
     HttpClientModule,
-    UserModule,
+   // UserModule,
     AuthModule,
     AppRoutingModule,
     FormsModule
