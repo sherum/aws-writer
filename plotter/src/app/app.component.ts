8a06820aa9037338fc39a790b0b159481c470de5
davidwoolery <sheurm@gmail.com>
2023-08-13 06:34:00 -0500

    update support classes

--- plotter/src/app/app.component.ts
+++ plotter/src/app/app.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
 import {BackendService} from "./services/backend.service";
 import {Observable} from "rxjs";
+import {CognitoService} from "./auth/cognito.service";
 
 @Component({
   selector: 'app-root',
@@ -9,23 +10,29 @@ import {Observable} from "rxjs";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
+  constructor(private router: Router,
+              private cognitoService: CognitoService) {
+    this.isAuthenticated = false;
+  }
+
+
+
   selectedStory:boolean = false;
 
-  constructor(private router: Router, private backend:BackendService) {}
+   isAuthenticated: boolean;
 
-  ngOnInit() {
-   this.router.navigate([{outlets: {details: null}}]);
-
+  public ngOnInit(): void {
+    this.cognitoService.isAuthenticated()
+    .then((success: boolean) => {
+      this.isAuthenticated = success;
+    });
   }
 
-  logOut(): void {
-    //delete session token
-    this.router.navigate(['/home']);
-  }
-
-  logIn():void{
-    //go to login page
-    this.router.navigate(['/login'])
+  public signOut(): void {
+    this.cognitoService.signOut()
+    .then(() => {
+      this.router.navigate(['/signIn']);
+    });
   }
 
 
